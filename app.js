9ffbc18ef496f76205659663c22eab49768c5bfd
Faustino Delpoggio <f.delpoggio@gmail.com>
2021-09-07 01:16:16 -0300

    Add carousel

---
+++ app.js
@@ -0,0 +1,35 @@
+// window.addEventListener('load', function(){
+//     new Glider(this.document.querySelector('.pormos-list', {
+//     slidesToShow: 1,
+//     dots: '.carousel-indicadores',
+//     draggable: true,
+//     arrows: {
+//         prev: '.anterior',
+//         next: '.siguiente'
+//     },
+//     responsive: [
+//         {
+//         // screens greater than >= 775px
+//         breakpoint: 775,
+//         settings: {
+//             // Set to `auto` and provide item width to adjust to viewport
+//             slidesToShow: 'auto',
+//             slidesToScroll: 'auto',
+//             itemWidth: 150,
+//             duration: 0.25
+//         }
+//         },{
+//         // screens greater than >= 1024px
+//         breakpoint: 1024,
+//         settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//             itemWidth: 150,
+//             duration: 0.25
+//         }
+//         }
+//     ]
+//     }));
+// });
+
+
