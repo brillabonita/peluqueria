83b67454c29573050cc7219ffa84660ce394d8bf
Faustino Delpoggio <f.delpoggio@gmail.com>
2021-09-11 20:31:37 -0300

    adding sections

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const tratamientos = document.querySelectorAll(".tratamientos");
 const titulo = document.querySelectorAll(".tratamiento-title");
 
 // #reparaciones > div.tratamiento-title > div > i.fas.fa-plus
-const mas = document.querySelector(".fa-plus");
-const menos = document.querySelector(".fa-minus");
 
 titulo.forEach((cadaTitulo, i) => {
   titulo[i].addEventListener("click", () => {
+    let mas = document.querySelector("body > div > div.menu-desplegable > div.tratamientos:nth-child(" + (i + 1 ) + ") > div.tratamiento-title > div > i.fas.fa-plus");
+    let menos = document.querySelector("body > div > div.menu-desplegable > div.tratamientos:nth-child(" + (i + 1 ) + ") > div.tratamiento-title > div > i.fas.fa-minus");
     if (!tratamientos[i].classList.contains("activo")) {
       tratamientos.forEach(() => {
         tratamientos[i].classList.remove("activo");
